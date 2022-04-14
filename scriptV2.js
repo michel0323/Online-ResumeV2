ff898e54ec49a85e993e2c663dbb5e37c6a3ad76
michel0323 <87438059+michel0323@users.noreply.github.com>
2022-04-14 02:38:11 -0400

    initialcommit

---
+++ scriptV2.js
@@ -0,0 +1,10 @@
+/*var btn= document.querySelector("pri");
+btn.onlick = () =>{
+    window.print();
+}*/
+
+document.getElementById("pri").addEventListener("click", myFunction);
+
+function myFunction() {
+    window.print();
+}
